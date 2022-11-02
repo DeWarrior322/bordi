5ed51c167b882e920f1c1098d1235185b3e358b3
DeWarrior322 <63714150+DeWarrior322@users.noreply.github.com>
2022-11-02 17:43:33 +0500

    Add files via upload

--- register.php
+++ register.php
@@ -11,13 +11,13 @@
 <body class="text-center">
     
 <main class="form-signin container-fluid col-md-3 mt-5">
-
-  <form action ="user.php" method="POST">
+  <!-- Форма регистрации -->
+  <form method="POST" action="">
     <img class="mb-4" src="images/Logo.png" alt="" width="150" height="120">
     <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
 
     <div class="form-floating mb-2">
-      <input type="name" name="userFIO" class="form-control" id="floatingInput" placeholder="FIO">
+      <input type="name" name="userName" class="form-control" id="floatingInput" placeholder="FIO">
       <label for="floatingInput">ФИО</label>
     </div>
 
@@ -27,11 +27,11 @@
     </div>
 
     <div class="form-floating mb-2">
-      <input type="number" class="form-control" id="floatingInput" placeholder="phoneNumber">
+      <input type="text" name="userPhone" class="form-control" id="floatingInput" placeholder="phoneNumber">
       <label for="floatingInput">Номер телефона</label>
     </div>
     <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input type="password" name="userPass" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Пароль</label>
     </div>
 
@@ -39,7 +39,50 @@
     <button class="w-60 btn btn-lg btn-primary" type="submit">Создать аккаунт</button>
   </form>
 
+<?php
+  // Запись данных в БД
+  if (isset($_POST['userName']) && isset($_POST['userEmail'])){
+    // Переменные с формы
+    $name = $_POST['userName'];
+    $email = $_POST['userEmail'];
+    $phone = $_POST['userPhone'];
+    $pass = $_POST['userPass'];
+    
+    // Параметры для подключения
+    $db_host = "localhost"; 
+    $db_user = "root"; // Логин БД
+    $db_password = "root"; // Пароль БД
+    $db_base = 'bordi_db'; // Имя БД
+    $db_table = "users"; // Имя Таблицы БД
+   
+    try {
+      
+        // Подключение к базе данных
+        $db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+        // Устанавливаем корректную кодировку
+        $db->exec("set names utf8");
+        // Собираем данные для запроса
+        $data = array( 'name' => $name, 'email' => $email, 'phone' => $phone, 'pass' => $pass); 
+        // Подготавливаем SQL-запрос
+        $query = $db->prepare("INSERT INTO $db_table (name, email, phone, pass) values (:name, :email, :phone, :pass)");
+        // Выполняем запрос с данными
+        $query->execute($data);
+        // Запишим в переменую, что запрос отрабтал
+        $result = true;
+    } catch (PDOException $e) {
+        // Если есть ошибка соединения или выполнения запроса, выводим её
+        print "Ошибка!: " . $e->getMessage() . "<br/>";
+    }
+    
+    if ($result) {
+    	echo "Успех. Информация занесена в базу данных"; 
+    }
+    
+  }
+?>
+
 </main>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
