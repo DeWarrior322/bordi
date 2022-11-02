d3e1586cb708799357252437a02807f0d08eb06f
DeWarrior322 <den.starkov55@gmail.com>
2022-11-02 21:21:05 +0500

    new file:   authorize.php 	new file:   blocks/header.php 	new file:   index.php 	new file:   register.php 	new file:   user.php

---
+++ register.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+</head>
+<body class="text-center">
+    
+<main class="form-signin container-fluid col-md-3 mt-5">
+  <!-- Форма регистрации -->
+  <form method="POST" action="">
+    <img class="mb-4" src="images/Logo.png" alt="" width="150" height="120">
+    <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
+
+    <div class="form-floating mb-2">
+      <input type="name" name="userName" class="form-control" id="floatingInput" placeholder="FIO">
+      <label for="floatingInput">ФИО</label>
+    </div>
+
+    <div class="form-floating mb-2">
+      <input type="email" name="userEmail" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Электронная почта</label>
+    </div>
+
+    <div class="form-floating mb-2">
+      <input type="text" name="userPhone" class="form-control" id="floatingInput" placeholder="phoneNumber">
+      <label for="floatingInput">Номер телефона</label>
+    </div>
+    <div class="form-floating mb-2">
+      <input type="password" name="userPass" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Пароль</label>
+    </div>
+
+    
+    <button class="w-60 btn btn-lg btn-primary" type="submit">Создать аккаунт</button>
+  </form>
+
+<?php
+  // Запись данных в БД
+  if (isset($_POST['userName']) && isset($_POST['userEmail'])){
+    // Переменные с формы
+    $name = $_POST['userName'];
+    $email = $_POST['userEmail'];
+    $phone = $_POST['userPhone'];
+    $pass = $_POST['userPass'];
+    
+    // Параметры для подключения
+    $db_host = "localhost"; 
+    $db_user = "root"; // Логин БД
+    $db_password = "root"; // Пароль БД
+    $db_base = 'bordi_db'; // Имя БД
+    $db_table = "users"; // Имя Таблицы БД
+   
+    try {
+      
+        // Подключение к базе данных
+        $db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+        // Устанавливаем корректную кодировку
+        $db->exec("set names utf8");
+        // Собираем данные для запроса
+        $data = array( 'name' => $name, 'email' => $email, 'phone' => $phone, 'pass' => $pass); 
+        // Подготавливаем SQL-запрос
+        $query = $db->prepare("INSERT INTO $db_table (name, email, phone, pass) values (:name, :email, :phone, :pass)");
+        // Выполняем запрос с данными
+        $query->execute($data);
+        // Запишим в переменую, что запрос отрабтал
+        $result = true;
+    } catch (PDOException $e) {
+        // Если есть ошибка соединения или выполнения запроса, выводим её
+        print "Ошибка!: " . $e->getMessage() . "<br/>";
+    }
+    
+    if ($result) {
+    	echo "Успех. Информация занесена в базу данных"; 
+    }
+    
+  }
+?>
+
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
