693be71092346abcee00c4130baabc46e46e729e
DeWarrior322 <63714150+DeWarrior322@users.noreply.github.com>
2022-11-09 15:01:30 +0500

    f

--- include/connect.php
+++ include/connect.php
@@ -1,15 +1,13 @@
 <?php
     // Параметры для подключения
-    $db_host = "localhost"; 
+    $db_host = "127.0.0.1:3308"; 
     $db_user = "root"; // Логин БД
     $db_password = "root"; // Пароль БД
     $db_base = 'bordi_db'; // Имя БД
-    $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
     try {
-        $pdo = new PDO("mysql:host=$db_host;dbname=$db_base;charset=utf8", $db_user, $db_password, $options);
+        $pdo = new PDO("mysql:host=$db_host;dbname=$db_base;charset=utf8", $db_user, $db_password);
     }catch(PDOException $i){
         die("Ошибка подключения к базе данных");
-
     }
 ?>
